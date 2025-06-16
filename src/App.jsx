6f76bbe40f29a56e8fdbf100a36f2fff22d2cef9
Sunita Thapa <sunitathapa1612@gmail.com>
2025-06-16 14:03:04 +0545

    feat: add scroll component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Layout } from "./components/Layout";
 import { Routing } from "./Routing";
+import { ScrollToTop } from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Layout>
         <Routing />
       </Layout>
